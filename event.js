967715c00f14825ef09ac219c0f692ba71db4818
Yajan-G <yajangurung@gmail.com>
2024-11-19 15:46:15 +0545

    eventeitter

--- event.js
+++ event.js
@@ -3,3 +3,14 @@ var rs = fs.createReadStream('./demofile.txt');
 rs.on('open', function () {
   console.log('The file is open');
 });
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+var myEventHandler = function () {
+    console.log('I hear a scream!');
+}
+
+eventEmitter.on('scream', myEventHandler);
+
+eventEmitter.emit('scream');
